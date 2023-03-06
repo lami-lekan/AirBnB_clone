19194f8a2727ceb282895adeaadb225e39a0cc4f
lami_lekan <olagbajuisrael11@gmail.com>
2023-03-06 17:40:41 +0000

    complete

--- models/base_model.py
+++ models/base_model.py
@@ -12,9 +12,8 @@ class BaseModel:
         """Initialize a new BaseModel.
         Args:
             *args (any): Unused.
-            **kwargs (dict): key/value pairs of attributes.
+            **kwargs (dict): Key/value pairs of attributes.
         """
-
         tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
@@ -40,11 +39,11 @@ class BaseModel:
         """
         rdict = self.__dict__.copy()
         rdict["created_at"] = self.created_at.isoformat()
-        rdict["update_at"] = self.updated_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
         rdict["__class__"] = self.__class__.__name__
         return rdict
 
     def __str__(self):
         """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__nam__
-        return "[{}] ({}) {}".format(clname.self.id, self.__dict__)
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
