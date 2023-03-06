fd95d4aa42277be80c14b73a8854cceb68bb62f8
lami_lekan <olagbajuisrael11@gmail.com>
2023-03-06 15:56:28 +0000

    TestConsole

---
+++ models/tests/test_console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines unittest for console.py.
+Unittest classes:
+    TestHBNBCommand_prompting
+    TestHBNBCommand_help
+    TestHBNBCommand_exit
+    TestHBNBCommand_create
+    TestHBNBCommand_show
+    TestHBNBCommand_all
+    TestHBNBCommand_destroy
+    TestHBNBCommand_update
+"""
+import os
+import sys
+import unittest
+from models import storage
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
+from io import StringIO
+from unittest.mock import patch
+
+
+class TestHBNBCommand_prompting(unittest.TestCase):
+    """unittests for the testing prompting of the HBNB command interpreter."""
+
+    def test_promp_string
